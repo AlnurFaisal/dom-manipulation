9488e460a5ec629c99eaf97650e9ffe8f8fd217d
Alnur_Faisal <alnurfaisal@outlook.com>
2018-03-05 13:52:39 +0800

    Updated index page to allow dom manipulation on click, submit and mouseover

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 var greeting_div = document.getElementById("greeting");
+var myForm = document.getElementsByTagName("form")[0];
+var yahoo = document.getElementById("yahoo");
+console.log(greeting_div);
 
 greeting_div.addEventListener("click", function() {
   greeting_div.textContent = "Howdy world!";
+  if(greeting_div.classList.contains("funky")){
+    greeting_div.classList.remove("funky");  
+  } else {
+    greeting_div.classList.add("funky");
+  }
 });
+
+greeting_div.addEventListener("mouseover", function(){
+  greeting_div.textContent = "Alnur Faisal!!";
+});
+greeting_div.addEventListener("mouseout", function(){
+  greeting_div.textContent = "Howdy world!";
+});
+myForm.addEventListener("submit", function(e){
+  e.preventDefault();
+});
+yahoo.addEventListener("click", function(e){
+  e.preventDefault();
+}); 
